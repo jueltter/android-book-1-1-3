536d88de0354a7d59621ede7b2bc59ca227db36f
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-15 10:08:24 -0500

    add webView

--- app/src/main/java/ec/dev/samagua/android_book_1_1_3/MainActivity.kt
+++ app/src/main/java/ec/dev/samagua/android_book_1_1_3/MainActivity.kt
@@ -1,47 +1,17 @@
 package ec.dev.samagua.android_book_1_1_3
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ec.dev.samagua.android_book_1_1_3.ui.theme.Androidbook113Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Androidbook113Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Androidbook113Theme {
-        Greeting("Android")
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
     }
 }
